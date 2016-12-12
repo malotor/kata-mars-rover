e0df6a13fefe4a1a3f35a7a0b17f56d526eb506a
Manel <malotor@gmail.com>
2016-12-12 23:07:45 +0100

    Step 2 red

--- spec/RoverSpec.php
+++ spec/RoverSpec.php
@@ -9,11 +9,17 @@ use Prophecy\Argument;
 class RoverSpec extends ObjectBehavior
 {
 
-    function it_should_have_with_initial_postion()
+    function it_should_have_with_initial_position()
     {
         $this->getCordX()->shouldReturn(0);
         $this->getCordY()->shouldReturn(0);
     }
 
+    function it_should_be_created_with_a_diferent_initials_positions()
+    {
+        $this->beConstructedWith(3,4);
 
+        $this->getCordX()->shouldReturn(3);
+        $this->getCordY()->shouldReturn(4);
+    }
 }

3787762e32c6eead29b41606dcf115f32739ee73
Manel <malotor@gmail.com>
2016-12-12 23:10:08 +0100

    Step 2 -> G

--- src/Rover.php
+++ src/Rover.php
@@ -5,14 +5,23 @@ namespace Prosodie\MarsRover;
 class Rover
 {
 
+    private $x;
+    private $y;
+
+    public function __construct($cordX = 0, $cordY = 0)
+    {
+        $this->x = $cordX;
+        $this->y = $cordY;
+    }
+
     public function getCordX()
     {
-        return 0;
+        return $this->x;
     }
 
     public function getCordY()
     {
-        return 0;
+        return $this->y;
     }
 
 }
